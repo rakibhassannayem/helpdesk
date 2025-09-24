a0f7dddf074f7596bc21ce5dfc4ee80e14a66e3f
rakibhassannayem <rakibhassannayem@gmail.com>
2025-09-24 11:31:04 +0600

    card designed

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,20 @@ function App() {
   return (
     <>
       <Navbar></Navbar>
-      <h1>Root</h1>
+      <div className="grid grid-cols-3 gap-5 my-12">
+        <div className="rounded-md text-white h-[200px] bg-gray-500 flex flex-col items-center justify-center">
+          <h1 className="font-bold text-4xl">Pending</h1>
+          <p className="font-bold text-2xl">10</p>
+        </div>
+        <div className="rounded-md text-white h-[200px] bg-purple-500 flex flex-col items-center justify-center">
+          <h1 className="font-bold text-4xl">Submitted</h1>
+          <p className="font-bold text-2xl">10</p>
+        </div>
+        <div className="rounded-md text-white h-[200px] bg-teal-500 flex flex-col items-center justify-center">
+          <h1 className="font-bold text-4xl">Reviewed</h1>
+          <p className="font-bold text-2xl">10</p>
+        </div>
+      </div>
       <Footer></Footer>
     </>
   )
