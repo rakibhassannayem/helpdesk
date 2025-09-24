0e09569e0d817178daa79eeb70729007b5d8fc90
rakibhassannayem <rakibhassannayem@gmail.com>
2025-09-24 10:53:59 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Footer from "./components/Footer"
+import Navbar from "./components/Navbar"
+
+function App() {
+  return (
+    <>
+      <Navbar></Navbar>
+      <h1>Root</h1>
+      <Footer></Footer>
+    </>
+  )
+}
+
+export default App
